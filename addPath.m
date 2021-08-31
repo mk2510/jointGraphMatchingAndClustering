b289f22ed78daf2ef0bd4d6dd94ee266fd3ff69f
Maximilian Krahn <mkrahn@mpi-inf.mpg.de>
2021-08-31 15:07:51 +0200

    change add path to right locations

--- addPath.m
+++ addPath.m
@@ -1,16 +1,15 @@
 function addPath
 % Add folders of predefined functions into matlab searching paths.
-%
-% History
-%   create  -  Feng Zhou (zhfe99@gmail.com), 03-20-2009
-%   modify  -  Feng Zhou (zhfe99@gmail.com), 05-07-2013
-
 global footpath;
 footpath = cd;
 
-addpath(genpath([footpath '/fgm-master/src']));
-addpath(genpath([footpath '/fgm-master/lib']));
-addpath(genpath([footpath '/fgm-master/PMSDP_method']));
 addpath(genpath([footpath '/PMSDP_relaxations']));
-% random seed generation
-% RandStream.setDefaultStream(RandStream('mt19937ar', 'seed', sum(100 * clock)));
+
+% change those dir if your installations locations of the packages are 
+% different
+addpath(genpath([footpath '/Mosek']));
+addpath(genpath([footpath '/toolbox_graph']));
+addpath(genpath([footpath '/YALMIP']));
+
+
+end
---
+++ syntheticData.m
@@ -0,0 +1,145 @@
+addPath;
+
+clear variables;
+prSet(3);
+
+%% save flag
+svL = 1; % change svL = 1 if you want to re-run the experiments.
+
+%% algorithm parameter
+tagAlg = 2;
+[~, algs] = gmPar(tagAlg);
+
+%% run 1 (perfect graphs, no noise)
+tagSrc = 2;
+[~, val1s] = cmumAsgPair(tagSrc);
+
+runPar = mod(11, 10);
+imgPar = floor(11/10);
+
+wsRun1 = syntheticDataRunner(tagSrc, tagAlg, 'svL', svL);
+fsta = wsRun1.Acc;
+plot_data = fsta(end-2:end,2:2:end-1);
+plot_data = plot_data .^(1/3);
+
+figure('color', 'w','DefaultAxesFontSize',16)
+subplot(2,1,1)
+
+plot_data([1 3], :) = plot_data([3 1], :);
+
+h = plot(plot_data');
+set(h,{'LineWidth'},{3;1.5;3});
+set(h,{'Marker'}, {'s';'o';'*'});
+set(gcf,'Position',[10 10 475 300])
+legend({'FGM-D + kmeans','SDP + Clustering','Our'}...
+    ,'Location','southwest','Orientation','horizontal','Box','off')
+xlabel('\sigma = 0.15 * x')
+set(gcf,'Position',[10 10 475 600])
+ylabel('a_m')
+xlim([1 8])
+
+plot_data2 = fsta(2:3,2:2:end);
+plot_data2 = plot_data2(1,:) .* plot_data2(2,:);
+plot_data2 = plot_data2 .^(1/2);
+
+plot_data3 = fsta(5:6,2:2:end);
+plot_data3 = plot_data3(1,:) .* plot_data3(2,:);
+plot_data3 = plot_data3 .^(1/2);
+
+plot_data4 = fsta(8:9,2:2:end);
+plot_data4 = plot_data4(1,:) .* plot_data4(2,:);
+plot_data4 = plot_data4 .^(1/2);
+
+plt_data = [plot_data4;plot_data3; plot_data2];
+subplot(2,1,2)
+h = plot(plt_data');
+xlim([1 10])
+set(h,{'LineWidth'},{3;1.5;3});
+set(h,{'Marker'}, {'s';'o';'*'});
+xlabel('\sigma = 0.15 * x')
+ylabel('a_c')
+xlim([1 8])
+
+
+wsRun2 = syntheticRunnerBig(1, imgPar, imgPar, tagSrc, tagAlg, 'svL', svL);
+
+fsta = wsRun2.Acc;
+plot_data =  fsta(end-2:end,2:end);
+plot_data = plot_data .^(1/3);
+
+plot_data([1 3], :) = plot_data([3 1], :);
+
+figure('Color', 'w','DefaultAxesFontSize',18)
+subplot(2,1,1)
+h = plot(1:10, plot_data');
+xlim([1 10])
+set(h,{'LineWidth'},{3;3;3});
+set(h,{'Marker'}, {'+';'o';'*'});
+xlabel('\sigma = 0.05 * x')
+ylabel('a_m')
+set(gcf,'Position',[10 10 475 600])
+legend({'FGM-D + kmeans', 'SGM', 'Our'}...
+    ,'Location','southwest','Orientation','horizontal','Box','off')
+
+
+plot_data2 = fsta(2:3,2:end);
+plot_data2 = plot_data2(1,:) .* plot_data2(2,:);
+plot_data2 = plot_data2 .^(1/2);
+
+plot_data3 = fsta(5:6,2:end);
+plot_data3 = plot_data3(1,:) .* plot_data3(2,:);
+plot_data3 = plot_data3 .^(1/2);
+
+plot_data4 = fsta(8:9,2:end);
+plot_data4 = plot_data4(1,:) .* plot_data4(2,:);
+plot_data4 = plot_data4 .^(1/2);
+
+plt_data = [plot_data4;plot_data3; plot_data2];
+subplot(2,1,2)
+h = plot(1:10,plt_data');
+xlim([1 10])
+set(h,{'LineWidth'},{3;3;3});
+set(h,{'Marker'}, {'+';'o';'*'});
+xlabel('\sigma = 0.05 * x')
+ylabel('a_c')
+
+wsRun3 = syntheticRunnerClustered(4, imgPar, tagSrc, tagAlg, 'svL', svL);
+
+fsta = wsRun3.Acc;
+plot_data = fsta(end-2:end,2:end-1);
+plot_data = plot_data .^(1/3);
+
+plot_data([1 3], :) = plot_data([3 1], :);
+
+figure('Color', 'w','DefaultAxesFontSize',18)
+subplot(1,2,1)
+h = plot(0:7,plot_data');
+set(h,{'LineWidth'},{3;3;3});
+set(h,{'Marker'}, {'+';'o';'*'});
+set(gcf,'Position',[10 10 950 300])
+xlabel('\sigma = 0.05 * x')
+xlim([0 7])
+ylabel('m- & c-acc')
+
+plot_data2 = fsta(2:3,2:end-1);
+plot_data2 = plot_data2(1,:) .* plot_data2(2,:);
+plot_data2 = plot_data2 .^(1/2);
+
+plot_data3 = fsta(5:6,2:end-1);
+plot_data3 = plot_data3(1,:) .* plot_data3(2,:);
+plot_data3 = plot_data3 .^(1/2);
+
+plot_data4 = fsta(8:9,2:end-1);
+plot_data4 = plot_data4(1,:) .* plot_data4(2,:);
+plot_data4 = plot_data4 .^(1/2);
+
+plt_data = [plot_data4;plot_data3; plot_data2];
+subplot(1,2,2)
+h = plot(0:7,plt_data');
+set(h,{'LineWidth'},{3;3;3});
+set(h,{'Marker'}, {'+';'o';'*'});
+xlim([0 7])
+xlabel('\sigma = 0.05 * x')
+ylabel('c-acc')
+legend({'FGM-D + kmeans','SGM', 'Our'}...
+    ,'Location','southwest','Orientation','horizontal','Box','off')
