52f579a07d106cb241d21dbc29a2ec9e9c77b254
Maximilian_Krahn <maximilian.krahn@icloud.com>
2022-01-10 21:43:34 +0200

    Update kron2vec.m

--- code/matching/objective_func/kron2vec.m
+++ code/matching/objective_func/kron2vec.m
@@ -1,20 +1,8 @@
+% 
+% original code by: Florian Bernard (f.bernardpi@gmail.com)
+% 
+
 function Aout = kron2vec(A, Br, Bc)
-% [Br,Bc] = size(B);
-% for A = kron(B,C): [kron2vec(A) = B(:)*C(:)']
-% 	D = size(A,1)/2;
-% 	N = D;
-	
-    % I think this function is wrong, index error?
-	% reshape
-	%Aout = [];
-	%for ii=1:Br
-	%	iIdx = (ii-1)*Br+1:ii*Br;
-	%	for jj=1:Bc
-	%		jIdx = (jj-1)*Bc+1:jj*Bc;
-	%		newRow = A(jIdx, iIdx);
-	%		Aout = [Aout; newRow(:)'];
-	%	end
-    %end
 	
     Aout = [];
 	for ii=1:Bc
@@ -25,19 +13,5 @@ function Aout = kron2vec(A, Br, Bc)
 			Aout = [Aout; newRow(:)'];
 		end
 	end
-    
-    
-% 	D = size(A,1)/2;
-% 	N = D;
-% 	
-% 	% reshape
-% 	Aout = [];
-% 	for ii=1:D
-% 		iIdx = (ii-1)*N+1:ii*N;
-% 		for jj=1:D
-% 			jIdx = (jj-1)*N+1:jj*N;
-% 			newRow = A(jIdx, iIdx);
-% 			Aout = [Aout; newRow(:)'];
-% 		end
-% 	end
+   
 end
